10165b1c448cc6502c9783cc1e816e2aaa8098cd
makhnev-a <ray.makhnev@gmail.com>
2020-05-22 17:41:37 +0300

    useState init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  let numbers = [1, 2, 3];
+
+  const addNumber = () => {
+    numbers.push(4);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          numbers.map(num => <li key={num}>{num}</li>)
+        }
+      </ul>
+      <button onClick={addNumber}>Введите число</button>
     </div>
   );
 }
